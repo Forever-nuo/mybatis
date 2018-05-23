16598815620e1f1cb7022be05a2ef1621a0a65ed
Forever丶诺 <857173891@qq.com>
2018-05-23 08:51:03 +0800

    修改Controller中注入service,并使属性小写

---
+++ src/main/java/com/forever/mybatisPlus/Hello.java
@@ -0,0 +1,12 @@
+package com.forever.mybatisPlus;
+
+/**
+ * @author: Forever丶诺
+ * @date: 2018/5/7 10:34
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
